f0c26daa7febfa1d18008487a883e5bf8c96d1a3
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-03-23 14:33:49 +0200

    Self_Practice

---
+++ Loops/Loops.js
@@ -0,0 +1,27 @@
+//the while loop
+
+while (condition) {
+    //code
+    //so-called "loop body"
+}
+
+//do...while loop
+do {
+    //loop body
+} while (condition)
+
+let i = 0;
+
+do {
+    alert(i);
+    i++;
+} while (i < 3);
+
+//the for loop
+for(begin, condition, step) {
+    //....loop body...
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
