dbfeba8f57f9991adae82fc097285886bc980d4a
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-06 13:04:16 +0200

    self_practice

--- node.js/lesson_1_node/built_in_modulea/os_module/main.js
+++
@@ -1,9 +0,0 @@
-const os = require("os")
-
-// info about currnet user
-const user = os.userInfo();
-console.log(user)
-
-// method return the system uptime in seconds
-console.log(`The system uptime is ${os.uptime()} seconds`);
-
