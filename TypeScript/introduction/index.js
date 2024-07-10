ce548e1d89a53803df3b4c5c062f450035def3a7
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-10 13:24:21 +0200

    small changes

--- TypeScript/introduction/index.js
+++
@@ -1,9 +0,0 @@
-const menu = [
-    {name: "Margherita", price: 8},
-    {name: "Pepperoni", price: 10},
-    {name: "Hawaiian", price: 10},
-    {name: "Veggie", price: 9}
-]
-
-const cashInRegister = 100
-const orderQueue = []
