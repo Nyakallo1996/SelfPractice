53ffb7bc582645a96f4d538cdfc4ead99cc119cc
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-11 10:07:33 +0200

    small changes

---
+++ TYPESCRIPT-TUT/tutorial.js
@@ -0,0 +1,13 @@
+"use strict";
+//defining variables
+/*let id: number = 5;//definig number
+let company: string = "Sol tech";//defining string
+let isPublished: boolean = true;//defining boolen
+
+let ids: number[] = [1, 2, 3, 4, 5];//defining array
+let x: any = "pedro";
+let xArr: any[] = ["pedro", 0, true];*/
+const concatenateValues = (a, b) => {
+    return a + b;
+};
+console.log(concatenateValues("Hello", "world"));
---
+++ TYPESCRIPT-TUT/tutorial.ts
@@ -0,0 +1,11 @@
+interface UserInterface {
+    id: number;
+    name: string;
+    age: number;
+}
+
+const User = {
+    id: 2,
+    name: "Pedro",
+    age: 22
+};
