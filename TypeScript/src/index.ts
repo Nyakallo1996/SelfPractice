61aebada2a7b438ec4c5485301ba0cd973fa9ed4
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-12 22:28:38 +0200

    small changes

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -11,6 +11,10 @@ let mySize: Size = Size.Medium;
 console.log(mySize);
 
 //functions
-function calculateTax(income: number) : number {
-    return 0;
+function calculateTax(income: number, taxYear: number) : number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
 }
+
+calculateTax(10_000, 2022)
