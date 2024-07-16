7a22199a62ec5d642049f2ba054b76291de4421e
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-16 21:41:42 +0200

    small changes

--- TypeScript/tutorial.js
+++ TypeScript/tutorial.js
@@ -6,9 +6,16 @@ let isPublished: boolean = true;
 
 let ids: number[] = [1, 2, 30];
 let sds: string[] = ["hi", "Hello"];
-let anny: any[] = ["Nyakallo", 1, false];*/
-const concatenateValues = (a, b) => {
-    return a + b;
-};
+let anny: any[] = ["Nyakallo", 1, false];
+
+const concatenateValues = (a: string, b: string): string => {
+    return a + b ;
+}
+
 console.log(concatenateValues("Hello", "World"));
-console.log(concatenateValues(5, 15));
+console.log(concatenateValues(5, 15));*/
+const User = {
+    id: 2,
+    name: "Nyakallo",
+};
+console.log(User.age);
--- TypeScript/tutorial.ts
+++ TypeScript/tutorial.ts
@@ -5,11 +5,27 @@ let isPublished: boolean = true;
 
 let ids: number[] = [1, 2, 30];
 let sds: string[] = ["hi", "Hello"];
-let anny: any[] = ["Nyakallo", 1, false];*/
+let anny: any[] = ["Nyakallo", 1, false];
 
 const concatenateValues = (a: string, b: string): string => {
     return a + b ;
 }
 
 console.log(concatenateValues("Hello", "World"));
-console.log(concatenateValues(5, 15));
+console.log(concatenateValues(5, 15));*/
+
+//interface and types
+interface UserInterface {
+    id: number;
+    name: string;
+    age?: number;  //? makes the age optional so the user is not required to enter it
+}
+
+
+const User: UserInterface = {
+    id: 2,
+    name: "Nyakallo",
+    
+};
+
+console.log(User.age);
