5306458fb75c034630cd454658db8a48b1fe3fac
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-16 21:32:54 +0200

    small changes

--- TypeScript/tutorial.js
+++ TypeScript/tutorial.js
@@ -1,3 +1,4 @@
+"use strict";
 //defining variables
 /*let id: number = 5;
 let company: string = "Account Number";
@@ -6,10 +7,8 @@ let isPublished: boolean = true;
 let ids: number[] = [1, 2, 30];
 let sds: string[] = ["hi", "Hello"];
 let anny: any[] = ["Nyakallo", 1, false];*/
-
-const concatenateValues = (a: string, b: string): string => {
-    return a + b ;
-}
-
+const concatenateValues = (a, b) => {
+    return a + b;
+};
 console.log(concatenateValues("Hello", "World"));
 console.log(concatenateValues(5, 15));
