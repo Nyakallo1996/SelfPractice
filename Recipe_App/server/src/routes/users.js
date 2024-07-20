8ac87fae32fd0e47fa6d3015547e7f0e54724945
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-20 21:25:10 +0200

    small changes

--- Recipe_App/server/src/routes/users.js
+++ Recipe_App/server/src/routes/users.js
@@ -7,10 +7,36 @@ const router = express.Router()
 
 router.post("/register", async (req, res) => {
     const {username, password} = req.body;
-    
     const user = await UserModel.findOne({username});
 
-    res.json(user);
+    if (user) {
+        return res.json({message: "User already exists!"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({ username, password: hashedPassword});
+    await newUser.save();
+
+    res.json({message: "User Registered successfully! "});
+});
+
+router.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+    const user = await UserModel.findOne({username});
+
+    if (!user) {
+        return res.json({message: "User Doesnt Exists"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.json({message: "Username or Password is incorrect"});
+    }
+
+    const token = jwt.sign({id: user._id}, "secret");
+    res.json({token, userID: user._id});
 })
 
 
