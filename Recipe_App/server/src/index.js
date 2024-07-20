b869016ed67b6770e310d44f6414b71cb8e5d007
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-20 19:01:33 +0200

    small changes

--- Recipe_App/server/src/index.js
+++ Recipe_App/server/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { userRouter } from "./routes/users.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(
   "mongodb+srv://lekgoabamahlaks:8j8Z6o5PUn4SA6Kx@cluster10.2wesq9t.mongodb.net/cluster10?retryWrites=true&w=majority&appName=Cluster10"
