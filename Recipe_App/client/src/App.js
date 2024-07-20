30b09fed6286ecb3346b4863e35f617f8aff1e98
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-20 21:45:33 +0200

    small changes

--- Recipe_App/client/src/App.js
+++ Recipe_App/client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Home } from './pages/home';
+import { Auth } from './pages/auth';
+import { Create } from './pages/create';
+import { Save } from './pages/save';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/create" element={<Create/>} />
+          <Route path="/save" element={<Save/>} />
         </Routes>
       </Router>
     </div>
