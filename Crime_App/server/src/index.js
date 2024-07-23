00a0b25a7c78eaba441a6a576769a527a09a5bf3
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-23 21:03:35 +0200

    small changes

--- Crime_App/server/src/index.js
+++ Crime_App/server/src/index.js
@@ -3,13 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import { userRouter } from "./routes/users.js";
-
+import { postsRouter } from "./routes/post.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/posts", postsRouter);
 
 mongoose.connect(
   "mongodb+srv://mahlakametsakabelo:2ZNZojXEDRcs4Wvs@crimeapp.nfzldo7.mongodb.net/?retryWrites=true&w=majority&appName=Crimeapp"
--- Crime_App/server/src/routes/post.js
+++ Crime_App/server/src/routes/post.js
@@ -2,4 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import { PostModel } from "../models/Post.js";
 
+const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        const response = await PostModel.find({});
+        res.json(response)
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+export { router as postsRouter };
