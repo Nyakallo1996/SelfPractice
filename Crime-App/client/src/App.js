0de29593dc2f7b8cf0a6886dab0e0f8fca40e5ec
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-07-28 22:45:42 +0200

    small changes

--- Crime-App/client/src/App.js
+++ Crime-App/client/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          Crime_Posts
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/07/Screenshot-2024-07-25-at-1.57.32%E2%80%AFPM.jpg?w=1024"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Full House</h2>
+          <p className="info">
+            <a className="author">Nyakallo</a>
+            <time>2024-07-28 20:30</time>
+          </p>
+          <p className="summary">
+            todayIt starts innocuously enough, with a father talking about how
+            much his daughter loves Sydney McLaughlin-Levrone (she “might even
+            be the world’s number one Sydney fan”). Apparently, she wants to
+            write the Olympic gold medalist a fan letter and needs her dad’s
+            help — and that’s where things take a turn.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/07/Screenshot-2024-07-25-at-1.57.32%E2%80%AFPM.jpg?w=1024"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Full House</h2>
+          <p>
+            todayIt starts innocuously enough, with a father talking about how
+            much his daughter loves Sydney McLaughlin-Levrone (she “might even
+            be the world’s number one Sydney fan”). Apparently, she wants to
+            write the Olympic gold medalist a fan letter and needs her dad’s
+            help — and that’s where things take a turn.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/07/Screenshot-2024-07-25-at-1.57.32%E2%80%AFPM.jpg?w=1024"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Full House</h2>
+          <p>
+            todayIt starts innocuously enough, with a father talking about how
+            much his daughter loves Sydney McLaughlin-Levrone (she “might even
+            be the world’s number one Sydney fan”). Apparently, she wants to
+            write the Olympic gold medalist a fan letter and needs her dad’s
+            help — and that’s where things take a turn.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
